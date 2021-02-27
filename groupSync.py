b1f25a07b364504aa5b54164458b5867478cd6ce
Gregory Dunlap <gregory.dunlap@fedex.com>
2021-02-27 13:42:45 -0600

    added group recursion

--- groupSync.py
+++ groupSync.py
@@ -25,8 +25,65 @@ def login_api(key, mds, domain):
     return response["sid"]
 #end of api
 
-def get_cma_contents(mds_ip, cma_ip, group_name):
+def parse_host(host_json):
     debug = 1
+
+    print(host_json['name'])
+    print(host_json['ipv4-address'])
+
+def parse_address_range(range_json):
+    debug = 1
+
+    print(range_json['name'])
+    print(range_json['ipv4-address-first'])
+    print(range_json['ipv4-address-last'])
+
+def parse_network(network_json):
+    debug = 1
+
+    print(network_json['name'])
+    print(network_json['subnet4'])
+    print(network_json['subnet-mask'])
+
+def get_group_contents(mds_ip, cma_sid, group_name):
+    debug = 1
+
+    # do stuff here
+    try:
+        group_info_response = apifunctions.api_call(mds_ip, "show-group", {"name" : group_name}, cma_sid)
+
+        print(group_info_response)
+
+        print("***********************************")
+        print(group_info_response['members'])
+
+        for i in range(len(group_info_response['members'])):
+            #print(group_info_response['members'][i])
+            
+
+            if(group_info_response['members'][i]['type'] == "host"):
+                parse_host(group_info_response['members'][i])
+            
+            if(group_info_response['members'][i]['type'] == "network"):
+                parse_network(group_info_response['members'][i])
+            
+            if(group_info_response['members'][i]['type'] == "address-range"):
+                parse_address_range(group_info_response['members'][i])
+
+            if(group_info_response['members'][i]['type'] == "group"):
+                print("group found", end=" ")
+                print(group_info_response['members'][i]['name'])
+                get_group_contents(mds_ip, cma_sid, group_info_response['members'][i]['name'])
+            print("\n---------")
+    except:
+        print("error getting group data")
+        
+
+def main():
+    debug = 1
+    print("begin")
+    mds_ip = "146.18.96.16"
+    cma1 = "146.18.96.25"
 
     key = {}
     with open('apirw-key.json', 'r') as f:
@@ -38,11 +95,11 @@ def get_cma_contents(mds_ip, cma_ip, group_name):
     
     sid = ""
     try:
-        sid = login_api(key['api-key'], mds_ip, cma_ip)
+        sid = login_api(key['api-key'], mds_ip, cma1)
         if(debug == 1):
             print("session id : " + sid)
 
-        # do stuff here
+        get_group_contents(mds_ip, sid, "Test-Group")
 
         time.sleep(5)
         logout_result = apifunctions.api_call(mds_ip, "logout", {}, sid)
@@ -53,15 +110,7 @@ def get_cma_contents(mds_ip, cma_ip, group_name):
         print("error with login")
         if(sid != ""):
             emergency_logout = apifunctions.api_call(mds_ip, "logout", {}, sid)
-
-def main():
-    print("begin")
-    mds_ip = "146.18.96.16"
-    cma1 = "146.18.96.25"
-
-    get_cma_contents(mds_ip, cma1, "cute-networks")
-
-    
+#end of main
 
 
 if __name__ == "__main__":
